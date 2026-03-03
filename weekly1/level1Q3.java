7bdc835e1df42bc61559ea860f0d2a7cc42be2a3
architpoonia09 <architpoonia09@gmail.com>
2026-03-03 09:13:17 +0530

    level1Q3

---
+++ weekly1/level1Q3.java
@@ -0,0 +1,9 @@
+package weekly1;
+
+public class level1Q3 {
+    public static void main(String[] args) {
+        double kilometers = 10.8;
+        double miles = kilometers * 1.6;
+        System.out.println("The distance " + kilometers + " km in miles is " + miles);
+    }
+}
